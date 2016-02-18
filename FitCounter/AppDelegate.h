2e0bf3211790bf3e5d2f147e1d3b17f21e5f429d
Kevin Dieu <kdieu001@gmail.com>
2016-02-18 00:18:40 -0800

    add new files

---
+++ FitCounter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FitCounter
+//
+//  Created by Kevin Dieu on 2/17/16.
+//  Copyright © 2016 Kevin Dieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
